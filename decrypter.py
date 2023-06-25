81b9f7ab11f1e5a7419025b95c3b9c89de395bab
Dayane Ferreira <110294040+daysilvaf@users.noreply.github.com>
2023-06-25 19:35:11 -0300

    Add files via upload

---
+++ decrypter.py
@@ -0,0 +1,24 @@
+import os
+import pyaes
+
+## Abrir o arquivo criptografado
+
+file_name = 'teste.txt.ransomwaretroll'
+file = open(file_name, 'rb')
+file_data = file.read()
+file.close()
+
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file_data)
+
+## Chave de descriptografia
+
+os.remove(file_name)
+
+## Criar um novo arquivo descriptografado
+
+new_file = 'teste.txt'
+new_file = open(f'{new_file}','wb')
+new_file.write(decrypt_data)
+new_file.close()
